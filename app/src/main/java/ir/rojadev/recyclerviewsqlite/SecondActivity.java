092c51b951dfde7bc08f57866dd724501d0392fc
ezatpanah <ezatpanah@gmail.com>
2018-12-24 13:08:31 +0330

    Initial commit

---
+++ app/src/main/java/ir/rojadev/recyclerviewsqlite/SecondActivity.java
@@ -0,0 +1,45 @@
+package ir.rojadev.recyclerviewsqlite;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SecondActivity extends AppCompatActivity {
+    DatabaseHelpher helpher;
+    List<DatabaseModel> dbList;
+    RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        helpher = new DatabaseHelpher(this);
+        dbList= new ArrayList<DatabaseModel>();
+        dbList = helpher.getDataFromDB();
+
+
+        mRecyclerView = (RecyclerView)findViewById(R.id.recycleview);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new RecyclerAdapter(this,dbList);
+        mRecyclerView.setAdapter(mAdapter);
+
+    }
+
+}
